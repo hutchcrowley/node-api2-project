bd4038bbb0009617f0c6067d3886ae61f2c22fb2
Hutch Crowley <tothecoda@gmail.com>
2020-04-13 15:53:56 -0500

    Update - implement all GET requests

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const welcomeRouter = require('./welcome/welcome-router')
 const postsRouter = require('./posts/posts-router')
 
 const server = express()
-const port = 4000
+const port = 3000
 
 server.use(express.json())
-server.use('./welcome', welcomeRouter)
-server.use('./posts', postsRouter)
+server.use('/', welcomeRouter)
+server.use('/posts', postsRouter)
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,17 +1,23 @@
 const express = require('express')
-const posts = require('../data/db')
+const db = require('../data/db.js')
 
-const postsRouter = express.Router()
+const router = express.Router()
 
-module.exports = postsRouter
+module.exports = router
 
-// first endpoint for handling get requests
-postsRouter.get('/', (req, res) => {
+// endpoint for handling get requests for the posts resource
+router.get('/', (req, res) => {
     console.log(req.query)
-    users
+    db
     .find()
     .then(posts => {
-        res.status(200).json(users)
+        if (posts) {
+            res.status(200).json(posts)
+        } else {
+            res.status(500).json({
+                message: 'Error: The posts information could not be retrieved',
+            })
+        }
     })
     .catch(error => {
         console.log(error)
@@ -20,3 +26,45 @@ postsRouter.get('/', (req, res) => {
         })
     })
 })
+
+// endpont for handling get repuests for post by id
+router.get('/:id', (req, res) => {
+    db.findById(req.params.id)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        }
+
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: 'Error: The post information could not be retrieved',
+        })
+    })
+})
+
+// endpoint for getting comments associated with a post specified by post id
+router.get('/:id/comments', (req, res) => {
+    db.findPostComments(req.params.id)
+    .then(comments => {
+        if (comments) {
+            res.status(200).json(comments)
+        } else {
+            res.status(404).json({
+                message: 'The post wit hthe specified ID does not exist.',
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: 'Error: The comments information could not be retrieved.'
+        })
+    })
+})
+
