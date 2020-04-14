98d3139da5c5733d26cf012929977289e8e454f2
Hutch Crowley <tothecoda@gmail.com>
2020-04-14 16:26:58 -0500

    Update put, delete request function, post need

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ const server = express()
 const port = 3000
 
 server.use(express.json())
-server.use(morgan())
-server.use('/', welcomeRouter)
-server.use('/posts', postsRouter)
+server.use(morgan('default'))
+server.use('/api', welcomeRouter)
+server.use('/api/posts', postsRouter)
+
+// middleware function to handle no route
+server.use((req, res) => {
+	res.status(404).json({
+		message: 'Route was not found',
+	})
+})
+
+// error handling middleware
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: 'Something went wrong',
+	})
+})
 
 server.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`)
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,7 +4,7 @@ const db = require('../data/db.js')
 const router = express.Router()
 
 // route handler for handling get requests for the posts resource
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
 	console.log(req.query)
 	db
 		.find()
@@ -17,16 +17,11 @@ router.get('/', (req, res) => {
 				})
 			}
 		})
-		.catch(error => {
-			console.log(error)
-			res.status(500).json({
-				message: 'Error retrieving the posts',
-			})
-		})
+		.catch(next)
 })
 
 // route handler for handling get repuests for post by id
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
 	db
 		.findById(req.params.id)
 		.then(post => {
@@ -38,16 +33,11 @@ router.get('/:id', (req, res) => {
 				})
 			}
 		})
-		.catch(err => {
-			console.log(err)
-			res.status(500).json({
-				message: 'Error: The post information could not be retrieved',
-			})
-		})
+		.catch(next)
 })
 
 // route handler for getting comments associated with a post specified by post id
-router.get('/:id/comments', (req, res) => {
+router.get('/:id/comments', (req, res, next) => {
 	db
 		.findPostComments(req.params.id)
 		.then(comments => {
@@ -59,43 +49,35 @@ router.get('/:id/comments', (req, res) => {
 				})
 			}
 		})
-		.catch(err => {
-			console.log(err)
-			res.status(500).json({
-				message: 'Error: The comments information could not be retrieved.',
-			})
-		})
+		.catch(next)
 })
 
 // route handler for adding a new post
-router.post('/', (req, res) => {
-	const { title, contents } = req.body
-	if (!title || !contents) {
+router.post('/', (req, res, next) => {
+	console.log(req)
+	const body = req.body
+	console.log('Request body in post request: ', body)
+	if (!body) {
 		res.status(400).json({
 			errorMessage: 'Please provide a title and contents for the post',
 		})
 	} else {
 		db
-			.insert(req.body)
+			.insert(body)
 			.then(id => {
 				db
 					.findById(id)
 					.then(post => {
-						return res.status(201).json(post)
+						res.status(201).json(post)
 					})
 					.catch(err => console.error('Error: ', err))
 			})
-			.catch(err => {
-				console.log(err)
-				res.status(500).json({
-					message: 'error: there was an error saving the post to the database',
-				})
-			})
+			.catch(next)
 	}
 })
 
 // route handler for adding a new comment
-router.post('/:id/comments', (req, res) => {
+router.post('/:id/comments', (req, res, next) => {
 	const { id } = req.params.id
 	const { text } = req.body
 	const comment = { ...req.body, post_id: id }
@@ -117,21 +99,15 @@ router.post('/:id/comments', (req, res) => {
 						.then(comment => {
 							res.status(201).json(comment)
 						})
-						.catch(err => {
-							res.status(500).json({
-								error: 'There was an error while saving the comment to the database',
-							})
-						})
+						.catch(next)
 				}
 			})
-			.catch(err => {
-				res.status(500).json(err)
-			})
+			.catch(next)
 	}
 })
 
 // route handler for editing a post
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
 	const post = req.body
 	const { id } = req.params
 	if (!req.body.title || !req.body.contents) {
@@ -150,17 +126,12 @@ router.put('/:id', (req, res) => {
 					})
 				}
 			})
-			.catch(err => {
-				console.log(err)
-				res.status(500).json({
-					error: 'The post information could not be modified',
-				})
-			})
+			.catch(next)
 	}
 })
 
 // route handler for deleting a post
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
 	const { id } = req.params
 	db
 		.remove(id)
@@ -175,12 +146,7 @@ router.delete('/:id', (req, res) => {
 				})
 			}
 		})
-		.catch(err => {
-			console.log(err)
-			res.status(500).json({
-				error: 'The post could not be removed',
-			})
-		})
+		.catch(next)
 })
 
 module.exports = router
